a61b8ceaa1c194c779107b04b38003ec0ad25983
210113yynim <tjalsdud9@gmail.com>
2019-05-24 19:20:55 +0900

    Update objectSensing.ino

--- arduino_Sensing/objectSensing.ino
+++ arduino_Sensing/objectSensing.ino
@@ -23,9 +23,7 @@ Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 void setup() {
   Serial.begin(9600);
-
   Serial.println("Adafruit MLX90614 test");  
-
   mlx.begin();  
 }
 
@@ -42,9 +40,10 @@ void loop() {
     Serial.print(mlx.readObjectTempC());
     Serial.println("*C");
   }
-  //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
-  //Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
 
   Serial.println();
   delay(1000);
 }
+
+  //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
+  //Serial.print("*F\tObject = "); Serial.print(mlx.readObjectTempF()); Serial.println("*F");
