6933c5de32ca877faffe75c4a0b85559a31e1d9b
Kang <tlsgur1747@gmail.com>
2019-05-27 14:28:50 +0900

    주석 추가
    경로 지정을 위한 추가 주석

--- drone_Control/Drone_RPI/_05_motor.cpp
+++ drone_Control/Drone_RPI/_05_motor.cpp
@@ -1,31 +1,30 @@
 #include "_00_drone.h"
 
 // 5. 함수 정의
-void init(
-	i2c_t& i2c, 
+void init(	//모터 초기화
+	i2c_t& i2c,
 	pca9685_t& pca9685) {
-			
-	pca9685.i2c_port = i2c.port;	
+
+	pca9685.i2c_port = i2c.port;
 	ioctl(pca9685.i2c_port, I2C_SLAVE, pca9685.i2c_addr);
 
-	setupAI(pca9685);	
+	setupAI(pca9685);
 	setFreq(pca9685, 1000);
 }//10
-void update(
-	pca9685_t& pca9685, 
-	motor_t& motor, 
+void update(	//결정 된 모터 스피드 적용
+	pca9685_t& pca9685,
+	motor_t& motor,
 	motor_speed_t& motor_speed) {
-	
+
 	int duty_cycle_a = (int)(motor_speed.a*4095.0/250);
 	int duty_cycle_b = (int)(motor_speed.b*4095.0/250);
 	int duty_cycle_c = (int)(motor_speed.c*4095.0/250);
 	int duty_cycle_d = (int)(motor_speed.d*4095.0/250);
-		
+
 	ioctl(pca9685.i2c_port, I2C_SLAVE, pca9685.i2c_addr);
-		
+
 	setDuty(pca9685, motor.a, duty_cycle_a);
 	setDuty(pca9685, motor.b, duty_cycle_b);
 	setDuty(pca9685, motor.c, duty_cycle_c);
 	setDuty(pca9685, motor.d, duty_cycle_d);
-}//10	
-
+}//10
