fb44a6aa4176f9dcd6a8383d5b983233c7beec45
210113yynim <tjalsdud9@gmail.com>
2019-05-28 11:42:24 +0900

    bluetooth_python_I2C

---
+++ arduino_Sensing/bluetooth.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+
+import serial
+from time import sleep
+
+bluetoothSerial = serial.Serial("/dev/rfcomm0", baudrate=9600)
+
+while not done:
+	
+	data = float(bluetoothSerial.readline())
+
+	if(data == 0):
+		print "location1: danger_object"
+        location = 1;
+	if(data == 1):
+		print "location1: danger_gas"
+        lacation = 1;
+	if(data == 2):
+		print "location2: danger_gas"
+        location = 2;
+	if(data == 3):
+		print "location3: danger_gas"
+        location = 3;
+	if(data == 4):
+		print "location4: danger_gas"
+        location = 4;
+
+serial.close();
+print "Bye!"
