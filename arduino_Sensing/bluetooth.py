207898e37d27b1830b85b840644383208fb502b9
210113yynim <tjalsdud9@gmail.com>
2019-05-28 19:47:03 +0900

    Update bluetooth.py

--- arduino_Sensing/bluetooth.py
+++ arduino_Sensing/bluetooth.py
@@ -5,29 +5,37 @@ from time import sleep
 
 bluetoothSerial = serial.Serial("/dev/rfcomm0", baudrate=9600)
 
+temp = 0
+location= 0
+
 while not done:
 	
+	temp = location
+
 	data = float(bluetoothSerial.readline())
 
 	if(data == 0):
-		print "location1: danger_object"
+		//print "location1: danger_object"
         location = 1
 
 	if(data == 1):
-		print "location1: danger_gas"
+		//print "location1: danger_gas"
         location = 1
 
 	if(data == 2):
-		print "location2: danger_gas"
+		//print "location2: danger_gas"
         location = 2
 
 	if(data == 3):
-		print "location3: danger_gas"
+		/print "location3: danger_gas"
         location = 3
 		
 	if(data == 4):
-		print "location4: danger_gas"
+		//print "location4: danger_gas"
         location = 4
+	
+	if(temp != location):
+		print location
 
 serial.close();
 print "Bye!"
