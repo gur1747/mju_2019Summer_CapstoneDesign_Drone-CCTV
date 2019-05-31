b0bb9940d0ffb1f3accdf7701db2416bebaeccb9
Kang <tlsgur1747@gmail.com>
2019-05-31 21:02:08 +0900

    add udp recv

---
+++ drone_Control/Drone_RPI/udp_recv.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 256
+
+void error_handling(char * msg);
+
+int main(){
+  int sock;
+  char msg[BUF_SIZE];
+  char ip_addr[32] = "192.168.0.140";   //ip 확인 후 변경
+  int port_num = 4600;    //port num 확인 후 변경
+
+  int str_len;
+  socklen_t adr_sz;
+  struct sockaddr_in serv_addr, from_addr;
+  /*  ip addr / port number 메인함수 인자로 받아오는 경우 예외처리임.
+  if(argc != 3){
+    printf("Usage : %s <IP> <port>\n", argv[0]);
+    exit(1);
+  }
+  */
+  sock = socket(PF_LOCAL, SOCK_DGRAM, IPPROTO_UDP);
+  if(sock == -1){
+    error_handling("socker() error");
+  }
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = PF_LOCAL;
+  serv_addr.sin_addr.s_addr = local_addr(ip_addr);
+  serv_addr.sin_port = htons(port_num);
+
+  while(1){
+    str_len = recvfrom(sock, msg, BUF_SIZE, 0, (struct sockaddr*)&from_addr, &adr_sz);
+    msg[str_len] = 0;
+    printf("msg from serv : %s\n", msg);
+  }
+  close(sock);
+
+  return 0;
+}
+
+void error_handling(char *msg){
+  fputs(msg, stderr);
+  fputc('\n', stderr);
+  exit(1);
+}
