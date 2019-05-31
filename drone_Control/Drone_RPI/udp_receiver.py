3a96ce23fd3396dadc54b2228d85cc59c32fb88b
Kang <tlsgur1747@gmail.com>
2019-06-01 02:03:39 +0900

    creat udp conn. source - python version
    sdr / rcvr

---
+++ drone_Control/Drone_RPI/udp_receiver.py
@@ -0,0 +1,13 @@
+import socket
+
+# socket create
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+recv_address = ('0.0.0.0', 9999)
+sock.bind(recv_address)
+
+data_size = 512
+
+data, sender = sock.recvfrom(data_size)
+sock.close()
---
+++ drone_Control/Drone_RPI/udp_sender.py
@@ -0,0 +1,9 @@
+import socket
+
+# connection create
+send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+dest = ("127.0.0.1", 9999)
+
+# send to dest
+send_sock.sendto(data, dest)
+send_sock.close()
