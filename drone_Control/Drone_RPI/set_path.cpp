4810c35c08047c3672ac98f706064aca4bc0f1db
Kang <tlsgur1747@gmail.com>
2019-06-03 01:59:12 +0900

    Update set_path.cpp
    add command func.

--- drone_Control/Drone_RPI/set_path.cpp
+++ drone_Control/Drone_RPI/set_path.cpp
@@ -1,4 +1,8 @@
 #include "_00_drone.h"
+#include <stdio.h>      //이하 헤더 4개는 kbhit 함수를 위함
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 i2c_t i2c;//1
 mpu6050_t mpu6050 = {
@@ -21,6 +25,8 @@ motor_t motor = { .a = 0, .b = 2, .c = 1, .d = 3, };//10
 pca9685_t pca9685 = { .i2c_addr = 0x40, };//10
 
 void myfunc();
+int kbhit(void);
+void command();
 
 int main() {
 
@@ -35,23 +41,27 @@ int main() {
 
         int i, j, k, l;
         for(i = 0; i <= 160; i++){
+                command();
                 throttle.value = i;
                 myfunc();
                 delay(10);
         }
 //t -> 160
         for(j = 0 ; j < 30; j++){
+                command();
                 throttle.value -= 1;
                 myfunc();
                 delay(10);
         }
 //t -> 130
         for(k = 0; k < 300; k++){
+                command();
                 myfunc();
                 delay(10);
         }
 //t -> 130
         for(l = 0; l < 26; l++){
+                command();
                 throttle.value -= 5;
                 myfunc();
                 delay(10);
@@ -73,5 +83,44 @@ void myfunc(){    //업데이트 루틴
         distribute(motor_speed, throttle, balancing_force);//8  //모터 속도 도출
         //check(hm10, throttle, target_angle);//9         //명령 수신 확인
         update(pca9685, motor, motor_speed);//10  //도출 된 모터 속도 적용
+}
 
+int kbhit(void){    //키보드 입력감지 함수 -> 감지 된 문자를 저장해둠
+
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(EOF != ch) {
+    ungetc(ch, stdin);
+    return 1;
+  }
+  return 0;
+}
+
+void command(){
+  if  ( kbhit() ){
+    c = getchar();
+    if(c == 'w'){
+      target_angle.pitch -= 2;
+    }else if(c == 's'){
+      target_angle.pitch += 2;
+    }else if(c == 'a'){
+      target_angle.roll -= 2;
+    }else if(c == 'd'){
+      target_angle.roll += 2;
+    }
+  }
 }
