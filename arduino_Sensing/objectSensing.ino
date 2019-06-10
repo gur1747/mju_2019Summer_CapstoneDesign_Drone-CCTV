859add23a6a24fbb20f6c52c4501446aeb2f62f1
210113yynim <tjalsdud9@gmail.com>
2019-06-10 22:00:51 +0900

    modify

--- arduino_Sensing/objectSensing.ino
+++ arduino_Sensing/objectSensing.ino
@@ -1,21 +1,3 @@
-/*************************************************** 
-  This is a library example for the MLX90614 Temp Sensor
-
-  Designed specifically to work with the MLX90614 sensors in the
-  adafruit shop
-  ----> https://www.adafruit.com/products/1748
-  ----> https://www.adafruit.com/products/1749
-
-  These sensors use I2C to communicate, 2 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 
@@ -44,7 +26,3 @@ void loop() {
   Serial.println();
   delay(1000);
 }
-
-
-  //Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempF()); 
-  //Serial.print("*F\tObject = "); Serial.przoqint(mlx.readObjectTempF()); Serial.println("*F");
